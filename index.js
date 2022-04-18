dab4d99d3bb136d39962e3467f7c83a2eac024bb
sammyatparachute <48567572+sammyatparachute@users.noreply.github.com>
2022-04-18 19:52:24 -0400

    Added vowel buying functionality and fixed 'bankrupt' behavior

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var correctGuesses = [];
 var canSpin = true;
 var potentialPoints;
 var playerOneScore = 0;
+var playerTwoScore = 0;
+var activePlayer = "playerOne";
+var guessingPhrase = false;
 const regex = new RegExp("[A-Z]");
 var wheelWedges = [
   "Bankrupt",
@@ -62,45 +65,171 @@ async function generateBoard() {
 }
 
 // ### Listen for keypress, reveal letter boxes when present, log "does not exist" when not present
+
 document.addEventListener("keypress", function (e) {
   let letterGuess = String.fromCharCode(e.keyCode).toUpperCase();
   document.getElementById(letterGuess).click();
 });
 
+function buyVowel(vowel) {
+    if (activePlayer == "playerOne") {
+        playerOneScore = playerOneScore - 250
+        playerOneScoreDisplay.textContent = playerOneScore;
+    }
+    else {
+        playerTwoScore = playerTwoScore - 250
+        playerTwoScoreDisplay.textContent = playerTwoScore;
+    }
+    if (guesses.find((element) => element == vowel)) {
+        alert(`${vowel} was already guessed`);
+      } else {
+        if (letterDiv.length > 0) {
+          letterDiv.forEach((element) => {
+            element.classList.remove("letter-hidden");
+            document
+              .getElementById(vowel)
+              .classList.add("keyboard-key-green");
+            correctGuesses.push(vowel);
+          });
+        } else {
+          document
+            .getElementById(vowel)
+            .classList.add("keyboard-key-gray");
+          if (activePlayer == "playerOne") {
+            activePlayer = "playerTwo";
+            playerTwoBox.style.borderColor = "green";
+            playerOneBox.style.borderColor = "white";
+          } else {
+            activePlayer = "playerOne";
+            playerOneBox.style.borderColor = "green";
+            playerTwoBox.style.borderColor = "white";
+          }
+        }
+        guesses.push(vowel);
+      }
+}
+
 function guess(letterGuess) {
   letterDiv = document.querySelectorAll(`.${letterGuess}`);
-  if (canSpin == true) {
-    alert("You need to spin the wheel first");
-  } else {
-    if (guesses.find((element) => element == letterGuess)) {
-      alert(`${letterGuess} was already guessed`);
-    } else {
-      if (letterDiv.length > 0) {
-        letterDiv.forEach((element) => {
-          element.classList.remove("letter-hidden");
-          document
-            .getElementById(letterGuess)
-            .classList.add("keyboard-key-green");
-          correctGuesses.push(letterGuess);
-          playerOneScore = playerOneScore + potentialPoints;
-          playerOneScoreDisplay.textContent = playerOneScore;
-        });
-      } else {
-        document.getElementById(letterGuess).classList.add("keyboard-key-gray");
-      }
-      guesses.push(letterGuess);
+  if (guessingPhrase == false) {
+    if (
+      letterGuess == "A" ||
+      letterGuess == "E" ||
+      letterGuess == "I" ||
+      letterGuess == "O" ||
+      letterGuess == "U"
+    ) {
+      buyVowel(letterGuess);
     }
-    canSpin = true;
+    else {
+      if (canSpin == true) {
+        alert("You need to spin the wheel first");
+      } else {
+        if (guesses.find((element) => element == letterGuess)) {
+          alert(`${letterGuess} was already guessed`);
+        } else {
+          if (letterDiv.length > 0) {
+            letterDiv.forEach((element) => {
+              element.classList.remove("letter-hidden");
+              document
+                .getElementById(letterGuess)
+                .classList.add("keyboard-key-green");
+              correctGuesses.push(letterGuess);
+              if (activePlayer == "playerOne") {
+                playerOneScore = playerOneScore + potentialPoints;
+                playerOneScoreDisplay.textContent = playerOneScore;
+              } else {
+                playerTwoScore = playerTwoScore + potentialPoints;
+                playerTwoScoreDisplay.textContent = playerTwoScore;
+              }
+            });
+          } else {
+            document
+              .getElementById(letterGuess)
+              .classList.add("keyboard-key-gray");
+            if (activePlayer == "playerOne") {
+              activePlayer = "playerTwo";
+              playerTwoBox.style.borderColor = "green";
+              playerOneBox.style.borderColor = "white";
+            } else {
+              activePlayer = "playerOne";
+              playerOneBox.style.borderColor = "green";
+              playerTwoBox.style.borderColor = "white";
+            }
+          }
+          guesses.push(letterGuess);
+        }
+        canSpin = true;
+      }
+    }
   }
 }
 
-function spin() {
-  if (canSpin == true) {
-    result = wheelWedges[Math.round(Math.random() * wheelWedges.length)];
-    spinResult.textContent = result;
-    potentialPoints = result;
-    canSpin = false;
-  } else {
-    alert("The wheel has already been spun");
+async function spin() {
+  if (guessingPhrase == false) {
+    if (canSpin == true) {
+      result = await wheelWedges[
+        Math.round(Math.random() * wheelWedges.length)
+      ];
+      spinResult.textContent = result;
+      potentialPoints = result;
+      if (result == "Loose a Turn") {
+        canSpin = true;
+        if (activePlayer == "playerOne") {
+          activePlayer = "playerTwo";
+          playerTwoBox.style.borderColor = "green";
+          playerOneBox.style.borderColor = "white";
+        } else {
+          activePlayer = "playerOne";
+          playerOneBox.style.borderColor = "green";
+          playerTwoBox.style.borderColor = "white";
+        }
+      } else if (result == "Bankrupt") {
+        canSpin = true;
+        if (activePlayer == "playerOne") {
+          playerOneScore = 0;
+          playerOneScoreDisplay.textContent = playerOneScore;
+          activePlayer = "playerTwo";
+          playerTwoBox.style.borderColor = "green";
+          playerOneBox.style.borderColor = "white";
+        } else {
+          playerTwoScore = 0;
+          playerTwoScoreDisplay.textContent = playerTwoScore;
+          activePlayer = "playerOne";
+          playerOneBox.style.borderColor = "green";
+          playerTwoBox.style.borderColor = "white";
+        }
+      } else {
+        canSpin = false;
+      }
+    } else {
+      alert("The wheel has already been spun");
+    }
   }
+}
+
+function phraseGuess() {
+  if (phraseGuessEntry.value.toUpperCase() == phrase.toUpperCase()) {
+    alert("Correct");
+    generateBoard();
+  } else {
+    alert("Incorrect");
+    if (activePlayer == "playerOne") {
+      activePlayer = "playerTwo";
+      playerTwoBox.style.borderColor = "green";
+      playerOneBox.style.borderColor = "white";
+    } else {
+      activePlayer = "playerOne";
+      playerOneBox.style.borderColor = "green";
+      playerTwoBox.style.borderColor = "white";
+    }
+  }
+  guessingPhrase = false;
+  phraseGuessDiv.style.display = "none";
+  phraseGuessEntry.value = "";
+}
+
+function revealPhraseGuess() {
+  phraseGuessDiv.style.display = "block";
+  guessingPhrase = true;
 }
